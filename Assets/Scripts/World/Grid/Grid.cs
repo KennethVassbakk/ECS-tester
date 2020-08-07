387ffb171334c9198574755491ad894379b4cf22
MrSpectacle <kenneth.vassbakk@gmail.com>
2020-08-07 17:40:52 +0200

    Comments.. so many comments.

--- Assets/Scripts/World/Grid/Grid.cs
+++ Assets/Scripts/World/Grid/Grid.cs
@@ -35,6 +35,8 @@ namespace dflike.World.Grid
             _layers = new Dictionary<int, TGridObject[,]>();
 
             // Populate grid
+            // This creates a TGridObject[,] for each of the layers (depth)
+            // it the inputs the generated TGridObject[,] into the _Layers(dictionary)
             for (var z = 0; z < depth; z++) {
                 var temp = new TGridObject[width, height];
                 for (var x = 0; x < width; x++) {
@@ -45,43 +47,95 @@ namespace dflike.World.Grid
                 }
 
                 _layers.Add(z, temp);
-
             }
 
             Debug.Log("Grid initialized");
         }
 
+        /// <summary>
+        /// Get the X dimension length of the grid.
+        /// </summary>
+        /// <returns>int Width</returns>
         public int GetWidth() {
             return _width;
         }
 
+        /// <summary>
+        /// Get the Y dimension length of the grid.
+        /// </summary>
+        /// <returns>int Height</returns>
         public int GetHeight() {
             return _height;
         }
 
+        /// <summary>
+        /// Get the Z dimension length of the grid.
+        /// This is the amount of layers that are present in the grid.
+        /// </summary>
+        /// <returns>int Z</returns>
         public int GetDepth() {
             return _depth;
         }
 
+        /// <summary>
+        /// Get the cell size of the grid items.
+        /// </summary>
+        /// <returns>int CellSize</returns>
         public float GetCellSize() {
             return _cellSize;
         }
 
+        /// <summary>
+        /// Get the World Position based on grid location.
+        /// </summary>
+        /// <param name="x">int X Location</param>
+        /// <param name="y">int Y location</param>
+        /// <returns>Vector 3 world location without Z</returns>
         public Vector3 GetWorldPosition(int x, int y) {
             return new Vector3(x, y) * _cellSize + _originPosition;
         }
 
+        /// <summary>
+        /// Get the World Position based on grid location (incl. z)
+        /// </summary>
+        /// <param name="location">Vector3 Location</param>
+        /// <returns>Vector3 Grid Location with Z</returns>
+        public Vector3 GetWorldPosition(Vector3 location)
+        {
+            return (location * _cellSize + _originPosition);
+        }
+
+        /// <summary>
+        /// Helper Function: Get XY location of a grid element based on a Vector3 (no Z)
+        /// </summary>
+        /// <param name="worldPosition">Vector3 WorldPosition</param>
+        /// <param name="x">x is returned from the function</param>
+        /// <param name="y">y is returned from the function</param>
         public void GetXy(Vector3 worldPosition, out int x, out int y) {
             x = Mathf.FloorToInt((worldPosition - _originPosition).x / _cellSize);
             y = Mathf.FloorToInt((worldPosition - _originPosition).y / _cellSize);
         }
 
+        /// <summary>
+        /// Helper function: Get XYZ location of a grid element based on a Vector3 (incl Z)
+        /// </summary>
+        /// <param name="worldPosition"></param>
+        /// <param name="x">x is returned from the function</param>
+        /// <param name="y">y is returned from the function</param>
+        /// <param name="z">z is returned from the function</param>
         public void GetXyz(Vector3 worldPosition, out int x, out int y, out int z) {
             x = Mathf.FloorToInt((worldPosition - _originPosition).x / _cellSize);
             y = Mathf.FloorToInt((worldPosition - _originPosition).y / _cellSize);
             z = Mathf.FloorToInt((worldPosition - _originPosition).z);
         }
 
+        /// <summary>
+        /// Set a grid object to a value
+        /// </summary>
+        /// <param name="x">int x Location</param>
+        /// <param name="y">int y Location</param>
+        /// <param name="z">int z Location</param>
+        /// <param name="value">Value</param>
         public void SetGridObject(int x, int y, int z, TGridObject value) {
             if (x < 0 || y < 0 || z < 0 || x >= _width || y >= _height || z >= _depth) return;
 
@@ -89,35 +143,72 @@ namespace dflike.World.Grid
             OnGridObjectChanged?.Invoke(this, new OnGridObjectChangedEventArgs { x = x, y = y, z = z });
         }
 
-        public void TriggerGridObjectChanged(int x, int y, int z) {
-            OnGridObjectChanged?.Invoke(this, new OnGridObjectChangedEventArgs { x = x, y = y, z = z });
-        }
-
+        /// <summary>
+        /// Set a grid object to a value based on a Vector3
+        /// </summary>
+        /// <param name="worldPosition">Vector3 WorldPosition</param>
+        /// <param name="value">value</param>
         public void SetGridObject(Vector3 worldPosition, TGridObject value) {
             GetXyz(worldPosition, out var x, out var y, out var z);
             SetGridObject(x, y, z, value);
         }
 
+        /// <summary>
+        /// Get a grid Object based on XYZ
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <returns>The object in the cell, or empty by default</returns>
         public TGridObject GetGridObject(int x, int y, int z) {
             if (x >= 0 && y >= 0 && z >= 0 && x < _width && y < _height && z < _depth) {
-
                 return _layers[z][x, y];
-            } else {
+            }
+            else 
+            {
                 return default(TGridObject);
             }
         }
 
+        /// <summary>
+        /// Get Grid object based on Vector3
+        /// </summary>
+        /// <param name="worldPosition">Vector3 World Position</param>
+        /// <returns>The Object in the cell, or empty by default</returns>
         public TGridObject GetGridObject(Vector3 worldPosition) {
-            GetXyz(worldPosition, out var x, out var y, out var z);
-            return GetGridObject(x, y, z);
+            if (worldPosition.x >= 0 && worldPosition.y >= 0 && worldPosition.z >= 0 && worldPosition.x < _width &&
+                worldPosition.y < _height && worldPosition.z < _depth)
+            {
+                GetXyz(worldPosition, out var x, out var y, out var z);
+                return GetGridObject(x, y, z);
+            }
+            else
+            {
+                return default(TGridObject);
+            }
         }
 
+        /// <summary>
+        /// Return a TGridObject[,] based on the defined Z layer
+        /// </summary>
+        /// <param name="z"></param>
+        /// <returns>The grid at the set depth(z)</returns>
         public TGridObject[,] GetGridLayer(int z) {
-            if (z >= 0 && z < _depth)
+            if (z >= 0 && z < _depth && _layers.ContainsKey(z))
                 return _layers[z];
             else {
                 return null;
             }
+        }
+
+        /// <summary>
+        /// Used to trigger an object change.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        public void TriggerGridObjectChanged(int x, int y, int z) {
+            OnGridObjectChanged?.Invoke(this, new OnGridObjectChangedEventArgs { x = x, y = y, z = z });
         }
     }
 }
