3150cd43270058305cb02f25a7f9f725d551484f
MrSpectacle <kenneth.vassbakk@gmail.com>
2020-08-09 15:47:03 +0200

    Simple camera controller. RectI helper.

---
+++ Assets/DFLite/Controllers/CameraController.cs
@@ -0,0 +1,77 @@
+﻿using DFLite.Helpers;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace DFLite.Controllers
+{
+    public class CameraController : MonoBehaviour
+    {
+        public const float PixelPerUnit = 32;
+        public float zoomDesired { get; protected set; }
+        public float ZoomMin { get; protected set; }
+        public float ZoomMax { get; protected set; }
+
+        //public float zoom => (Screen.height / (this.zoomDesired * CameraController.PIXEL_PER_UNIT) * 0.5f);
+        public int Zoom => Mathf.FloorToInt(zoomDesired * PixelPerUnit);
+
+        private PixelPerfectCamera _ppc;
+
+        public float Sensitivity { get; protected set; }
+        public Vector3 MousePosition { get; protected set; }
+
+        public RectI viewRect;
+        private Vector3 _lastMousePosition;
+        private Camera _camera;
+
+        private void Start() {
+            this._camera = Camera.main;
+            this.ZoomMin = .1f;
+            this.ZoomMax = 10f;
+            this.Sensitivity = 1f;
+            this.zoomDesired = 1f;
+            this._ppc = GetComponent<PixelPerfectCamera>();
+        }
+
+        private void Update() {
+            if (Input.GetMouseButtonDown(2))
+                _lastMousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            this.UpdateCamera();
+        }
+
+        private void UpdateCamera() {
+            if (Input.GetMouseButton(2)) {
+                MousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+                var diff = this._lastMousePosition - this.MousePosition;
+
+                if (Vector3.Distance(diff, Vector3.zero) > 0.05f) {
+                    this._camera.transform.Translate(diff * .8f);
+                    this.UpdateViewRect();
+                }
+            }
+
+            this.zoomDesired += this.zoomDesired * Input.GetAxis("Mouse ScrollWheel") * this.Sensitivity;
+            this.zoomDesired = Mathf.Clamp(this.zoomDesired, this.ZoomMin, this.ZoomMax);
+
+            if (Zoom - _ppc.assetsPPU == 0) return;
+            _ppc.assetsPPU = Mathf.FloorToInt(Zoom);
+            this.UpdateViewRect();
+        }
+
+        private void UpdateViewRect() {
+            // Creating local var for efficiency
+            var position = _camera.transform.position;
+
+            this.viewRect = new RectI(
+                new Vector2Int(
+                    Mathf.FloorToInt(position.x - _camera.orthographicSize * _camera.aspect),
+                    Mathf.FloorToInt(position.y - _camera.orthographicSize)
+                ), new Vector2Int(
+                    Mathf.FloorToInt(position.x - _camera.orthographicSize * _camera.aspect),
+                    Mathf.FloorToInt(position.y - _camera.orthographicSize)
+                ));
+
+            // Time to redraw the map!
+            // TODO: Reference the overwatch unit and redraw map!
+        }
+    }
+}
---
+++ Assets/DFLite/Helpers/RectI.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DFLite.Helpers
+{
+    /// <summary>
+    /// Represents a rectangle
+    /// from bottom left to top right.
+    /// </summary>
+    ///      ______.max
+    ///     |      |
+    ///     |      |
+    ///     .______|
+    ///   min
+    [System.Serializable]
+    public struct RectI
+    {
+        public Vector2Int min;
+        public Vector2Int max;
+
+        public int width => this.max.x - this.min.x;
+        public int height => this.max.y - this.min.y;
+        public int area => this.width * this.height;
+        public Vector2Int size => new Vector2Int(this.width, this.height);
+
+        /// <summary>
+        /// Create from min, max
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public RectI(Vector2Int min, Vector2Int max) {
+            this.min = min;
+            this.max = max;
+        }
+
+        /// <summary>
+        /// Create from min position, using width & height
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public RectI(Vector2Int min, int width, int height) {
+            this.min = min;
+            this.max = new Vector2Int(this.min.x + width, this.min.y + height);
+        }
+
+        /// <summary>
+        /// Enumerate over rects
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<Vector2Int> GetEnumerator() {
+            for (var x = this.min.x; x < this.max.x; x++) {
+                for (var y = this.min.y; y < this.max.y; y++) {
+                    yield return new Vector2Int(x, y);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clip this RectI inside another RectI
+        /// </summary>
+        /// <param name="other"></param>
+        public void Clip(RectI other) {
+            if (this.min.x < other.min.x) {
+                this.min.x = other.min.x;
+            }
+
+            if (this.max.x > other.max.x) {
+                this.max.x = other.max.x;
+            }
+
+            if (this.min.y < other.min.y) {
+                this.min.y = other.min.y;
+            }
+
+            if (this.max.y < other.max.y) {
+                this.max.y = other.max.y;
+            }
+        }
+
+        /// <summary>
+        /// Check if Vector2Int is inside our rectangle
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public bool Contains(Vector2Int v) {
+            return (
+                v.x >= this.min.x &&
+                v.y >= this.min.y &&
+                v.x < this.max.x &&
+                v.y < this.max.y
+            );
+        }
+
+        /// <summary>
+        /// Get the Rect HashCode
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode() {
+            return this.min.x + this.min.y * this.width + this.max.x * this.height + this.max.y;
+        }
+
+        /// <summary>
+        /// ToString Override
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() {
+            return "RectI(min: " + this.min + ", max: " + this.max + ", size: " + this.size + ", area: " + this.area +
+                   ")";
+        }
+    }
+}
