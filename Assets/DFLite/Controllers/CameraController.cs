9bc8faf1aa3578ca5b3eed6b9d80a3b4dfd7e7c3
MrSpectacle <kenneth.vassbakk@gmail.com>
2020-08-09 18:42:20 +0200

    Camera Extents & Scaling

--- Assets/DFLite/Controllers/CameraController.cs
+++ Assets/DFLite/Controllers/CameraController.cs
@@ -1,20 +1,19 @@
 ﻿using DFLite.Helpers;
+using System;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.Universal;
 
 namespace DFLite.Controllers
 {
     public class CameraController : MonoBehaviour
     {
-        public const float PixelPerUnit = 32;
-        public float zoomDesired { get; protected set; }
+        // ReSharper disable once InconsistentNaming
+        public const float PIXEL_PER_UNIT = 32;
+        public float ZoomDesired { get; protected set; }
         public float ZoomMin { get; protected set; }
         public float ZoomMax { get; protected set; }
 
-        //public float zoom => (Screen.height / (this.zoomDesired * CameraController.PIXEL_PER_UNIT) * 0.5f);
-        public int Zoom => Mathf.FloorToInt(zoomDesired * PixelPerUnit);
+        public float Zoom => (Screen.height / (this.ZoomDesired * CameraController.PIXEL_PER_UNIT) * 0.5f);
 
-        private PixelPerfectCamera _ppc;
 
         public float Sensitivity { get; protected set; }
         public Vector3 MousePosition { get; protected set; }
@@ -22,52 +21,64 @@ namespace DFLite.Controllers
         public RectI viewRect;
         private Vector3 _lastMousePosition;
         private Camera _camera;
+        private bool _updateViewRect;
+
 
         private void Start() {
             this._camera = Camera.main;
             this.ZoomMin = .1f;
             this.ZoomMax = 10f;
             this.Sensitivity = 1f;
-            this.zoomDesired = 1f;
-            this._ppc = GetComponent<PixelPerfectCamera>();
+            this.ZoomDesired = 1f;
+            UpdateViewRect();
         }
 
         private void Update() {
             if (Input.GetMouseButtonDown(2))
                 _lastMousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+
             this.UpdateCamera();
+
+            if (_updateViewRect) {
+                UpdateViewRect();
+            }
         }
 
         private void UpdateCamera() {
-            if (Input.GetMouseButton(2)) {
-                MousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-                var diff = this._lastMousePosition - this.MousePosition;
+            this.ZoomDesired += this.ZoomDesired * Input.GetAxis("Mouse ScrollWheel") * this.Sensitivity;
+            this.ZoomDesired = Mathf.Clamp(this.ZoomDesired, this.ZoomMin, this.ZoomMax);
 
-                if (Vector3.Distance(diff, Vector3.zero) > 0.05f) {
-                    this._camera.transform.Translate(diff * .8f);
-                    this.UpdateViewRect();
-                }
+            if (Math.Abs(Zoom - _camera.orthographicSize) > 0.01f) {
+                this._camera.orthographicSize = this.Zoom;
+                _updateViewRect = true;
+
             }
 
-            this.zoomDesired += this.zoomDesired * Input.GetAxis("Mouse ScrollWheel") * this.Sensitivity;
-            this.zoomDesired = Mathf.Clamp(this.zoomDesired, this.ZoomMin, this.ZoomMax);
+            if (!Input.GetMouseButton(2)) return;
+            MousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            var diff = this._lastMousePosition - this.MousePosition;
 
-            if (Zoom - _ppc.assetsPPU == 0) return;
-            _ppc.assetsPPU = Mathf.FloorToInt(Zoom);
-            this.UpdateViewRect();
+            if (!(Vector3.Distance(diff, Vector3.zero) > 0.01f)) return;
+            this._camera.transform.Translate(diff * .8f);
+            _updateViewRect = true;
         }
 
         private void UpdateViewRect() {
+            _updateViewRect = false;
             // Creating local var for efficiency
             var position = _camera.transform.position;
+            var currOrthoSize = Mathf.CeilToInt(_camera.orthographicSize);
+
+            //  Undone: When we're doing chunks, we should probably add an additional chunk size on each side
+            var orthoSize = currOrthoSize + Mathf.CeilToInt(currOrthoSize / 5f);
 
             this.viewRect = new RectI(
                 new Vector2Int(
-                    Mathf.FloorToInt(position.x - _camera.orthographicSize * _camera.aspect),
-                    Mathf.FloorToInt(position.y - _camera.orthographicSize)
+                    Mathf.FloorToInt(position.x - orthoSize * _camera.aspect),
+                    Mathf.FloorToInt(position.y - orthoSize)
                 ), new Vector2Int(
-                    Mathf.FloorToInt(position.x - _camera.orthographicSize * _camera.aspect),
-                    Mathf.FloorToInt(position.y - _camera.orthographicSize)
+                    Mathf.FloorToInt(position.x + orthoSize * _camera.aspect),
+                    Mathf.FloorToInt(position.y + orthoSize)
                 ));
 
             // Time to redraw the map!
